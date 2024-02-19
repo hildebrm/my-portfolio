61ef7ed46dacf8461f1dd94487132d60e51ef6c8
maxxHildebrand <mbhildebrand6@gmail.com>
2024-02-19 09:31:20 -0500

    Remove Github link from Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,6 @@ const Navbar = () => {
             <h1><Link to="/">Home</Link></h1>
             <div className="Links">
                 <Link to="/projects">Projects</Link>
-                <Link to="https://github.com/hildebrm" target="_blank" rel="noopener noreferrer">Github</Link>
                 <Link to="https://www.linkedin.com/in/maxwell-hildebrand-b9276328b/" target="_blank" rel="noopener noreferrer">LinkedIn</Link>
                 <Link to="https://app.joinhandshake.com/stu/users/34598327" target="_blank" rel="noopener noreferrer">Handshake</Link>
                 <a href={resume}>Resume</a>
